1f9fe262785685c2c6c1ee2da9f73f15c09544e6
Marco Szulik <marco.szulik@goodbabyint.com>
2023-02-27 14:43:56 +0100

    reference env keys for urls and client name in config

--- config/transmorpher.php
+++ config/transmorpher.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'client_name' => '',
+    'client_name' => env('TRANSMORPHER_CLIENT_NAME'),
 
     'api' => [
-        'url' => '',
+        'url' => env('TRANSMORPHER_API_URL'),
         'auth_token' => env('TRANSMORPHER_AUTH_TOKEN'),
         'callback_route' => 'transmorpher/callback'
     ],
 
     'public' => [
-        'url' => ''
+        'url' => env('TRANSMORPHER_PUBLIC_URL')
     ],
 ];
