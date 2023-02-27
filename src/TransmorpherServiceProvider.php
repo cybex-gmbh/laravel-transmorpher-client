29bc64048dca236cab3fa4c33d911ac706c6959a
Marco Szulik <marco.szulik@goodbabyint.com>
2023-02-27 13:52:44 +0100

    hardcode datetime for migrations

--- src/TransmorpherServiceProvider.php
+++ src/TransmorpherServiceProvider.php
@@ -17,7 +17,7 @@ class TransmorpherServiceProvider extends ServiceProvider
             ], 'transmorpher.config');
         }
 
-        $this->publishMigrations();
+        $this->loadMigrationsFrom(sprintf('%s/Migrations', __DIR__));
     }
 
     /**
@@ -27,39 +27,5 @@ class TransmorpherServiceProvider extends ServiceProvider
     {
         // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__ . '/../config/transmorpher.php', 'transmorpher');
-    }
-
-    /**
-     * Publish the package's migrations.
-     *
-     * @return void
-     */
-    protected function publishMigrations()
-    {
-        $this->publishMigration('CreateTransmorpherMediaTable', 'create_transmorpher_media_table.php');
-        // This has to be done after the CreateTransmorpherMediaTable migration.
-        $this->publishMigration('CreateTransmorpherProtocolsTable', 'create_transmorpher_protocols_table.php', 1);
-    }
-
-    /**
-     * Publish a single migration with a timestamp.
-     *
-     * @param string   $className
-     * @param string   $migrationName
-     * @param int|null $addSeconds
-     *
-     * @return void
-     */
-    protected function publishMigration(string $className, string $migrationName, int $addSeconds = null)
-    {
-        if (class_exists($className)) {
-            return;
-        }
-
-        $timestamp = date('Y_m_d_His', time() + $addSeconds);
-        $stub      = sprintf('%s/Migrations/%s', __DIR__, $migrationName);
-        $target    = $this->app->databasePath(sprintf('migrations/%s_%s', $timestamp, $migrationName));
-
-        $this->publishes([$stub => $target], 'transmorpher.migrations');
     }
 }
