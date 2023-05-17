6f9419f71ea3d3042701b00147553c33f0d1d81e
Marco Szulik <marco.szulik@goodbabyint.com>
2023-05-17 16:35:38 +0200

    wip add error icon display

--- src/resources/views/transmorpher-dropzone.blade.php
+++ src/resources/views/transmorpher-dropzone.blade.php
@@ -18,6 +18,13 @@
                 @endif
             </span>
             <div class="details">
+                <div class="error-display">
+                    <svg class="icon error-icon" viewBox="0 0 24 24" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 16H12.01M12 8V12M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z"/>
+                    </svg>
+                    <div class="card error-information">hallöchen</div>
+                </div>
                 @if ($isImage)
                     <a target="_blank" href="{{ $motif->getUrl() }}"><img src="{{ mix('icons/magnifying-glass.svg', 'vendor/transmorpher') }}" alt="Enlarge image" class="icon"></a>
                 @endif
