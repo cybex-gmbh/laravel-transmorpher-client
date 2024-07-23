0a05f99bb3931cc7466c8bfca51ffa4b012263cb
Marco Szulik <marco.szulik@goodbabyint.com>
2024-07-23 12:52:34 +0200

    return placeholderUrl when retrieving state and the media was deleted

--- src/Controller/UploadStateController.php
+++ src/Controller/UploadStateController.php
@@ -27,12 +27,18 @@ class UploadStateController
             $state = UploadState::ERROR->value;
         }
 
+        if ($state ?? $latestUpload?->state === UploadState::DELETED) {
+            $mediaUrls = ['placeholderUrl' => $transmorpherMedia->getMedia()->getPlaceholderUrl()];
+        } else {
+            $mediaUrls = $transmorpherMedia->getMedia()->getMediaUrls();
+        }
+
         return response()->json([
             'clientMessage' => $message ?? $latestUpload?->message,
             'state' => $state ?? $latestUpload?->state,
             'latestUploadToken' => $transmorpherMedia->latest_upload_token,
             'lastUpdated' => $latestUpload?->updated_at,
-            ...$transmorpherMedia->getMedia()->getMediaUrls()
+            ...$mediaUrls
         ]);
     }
 
