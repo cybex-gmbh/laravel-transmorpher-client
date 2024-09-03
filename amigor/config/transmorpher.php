5e76e32355cdbc573a75cc6403137d087511fbfe
Marco Szulik <marco.szulik@goodbabyint.com>
2024-09-03 08:52:50 +0200

    remove unnecessary web middleware in amigor transmorpher config

--- amigor/config/transmorpher.php
+++ amigor/config/transmorpher.php
@@ -3,9 +3,10 @@
 return [
     'client_name' => env('TRANSMORPHER_CLIENT_NAME'),
 
-    // The middleware applied to routes provided by this package.
-    // The SubstituteBindings middleware is always applied.
-    'routeMiddleware' => ['web'],
+    // The middlewares applied to routes provided by this package:
+    // - the "SubstituteBindings" middleware will be applied additionally.
+    // - "web" and "auth" middlewares will be applied when this is not set.
+    'routeMiddleware' => [],
 
     'api' => [
         // Optionally, specify the Transmorpher API version which should be used. For supported versions, check the SupportedApiVersion enum.
